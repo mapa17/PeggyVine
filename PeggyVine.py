fcd595be5a395026fc9ca0f2906ec569681b4471
Pasieka Manuel <mapa17@posgrado.upv.es>
2012-03-21 01:15:14 +0000

    Created basic project structure

---
+++ PeggyVine.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 20, 2012
+
+@author: Pasieka Manuel , mapa17@posgrado.upv.es
+'''
+
+from lfcShell import lfcShell
+
+if __name__ == '__main__':
+    s = lfcShell()
+    s.run()
+    pass
